76169c3174d629759551d2ff99ec91e97d94f86e
Torie Pham <tientp@uci.edu>
2018-08-17 16:14:08 -0700

    used  null for checking occupation

--- room.js
+++ room.js
@@ -4,13 +4,12 @@ class Room{
 	constructor(currentHotel){
 		this.occupant = null; //a customer
 		this.hotel = currentHotel;
-		this.isOccupied = false;
 	}
 	addOccupant( customer ){
 		this.occupant = customer;
 	}
 	checkIfOccupied(){
-		if(this.isOccupied===false){
+		if(this.occupant===null){
 			return false;
 		} else {
 			return true;
