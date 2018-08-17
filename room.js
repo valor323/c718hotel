6c3ff1cd49d1f7a035eb2915006e19e1081a271c
Torie Pham <tientp@uci.edu>
2018-08-17 16:03:03 -0700

    added checkIfOccupied function and added customer to room

--- room.js
+++ room.js
@@ -4,9 +4,17 @@ class Room{
 	constructor(currentHotel){
 		this.occupant = null; //a customer
 		this.hotel = currentHotel;
+		this.isOccupied = false;
 	}
 	addOccupant( customer ){
-
+		this.occupant = customer;
+	}
+	checkIfOccupied(){
+		if(this.isOccupied===false){
+			return false;
+		} else {
+			return true;
+		}
 	}
 	removeOccupant(){
 
