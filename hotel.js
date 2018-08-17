60b23d8bc859cc8ca347f533700b519484edcefb
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-08-17 15:26:19 -0700

    indentations

--- hotel.js
+++ hotel.js
@@ -8,15 +8,14 @@ class Hotel{
 	}
 	//make a customer object with the given name and add to a room.  need to add to a room with no occupants
 	checkInCustomer( name ){
-	// var customer = new Customer(name);//
 	}
 	checkOutCustomer(){
 
 	}
 	//make a room and add to rooms array
 	addRoom(){
-	var newRoom = new Room(this);
-	this.rooms.push(newRoom);
+		var newRoom = new Room(this);
+		this.rooms.push(newRoom);
 	}
 	//add staff to the hotel
 	hireStaff( name ) {
