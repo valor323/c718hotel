f0c1a6b7c4f2ff154edd8f341674c7f5292cd592
valor323 <caine101@mail.chapman.edu>
2018-08-17 17:37:43 -0700

    added Check if hungry function

--- customer.js
+++ customer.js
@@ -5,6 +5,17 @@ class Customer{
 		this.name = occupentsName;
 		this.room = null; //room we are currently
 		this.isHungry = false;
+		setInterval(this.checkIfHungry(),1000);
+	}
+	checkIfHungry(){
+		var random = Math.random();
+		if(random <= 0.05){
+			this.isHungry = true;
+			this.orderRoomService('Potatoes, Boil em, mash, em, stick em in a stew')
+			console.log('im staving')
+		}else{
+			 console.log('Im not hungry')
+		}
 	}
 	orderRoomService( food ){
 		this.room.getRoomService(food);
@@ -13,3 +24,6 @@ class Customer{
 		
 	}
 }
+
+var fido = new Customer('Fido');
+
